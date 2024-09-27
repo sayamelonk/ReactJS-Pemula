035bd9aa702ea48a0f78b8540ecb756c8375222c
Riki Melonk <rikymelonk00@gmail.com>
2024-09-28 04:19:46 +0700

    React Props Component dan List Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,35 @@ import Navigation from './components/Navigation'
 import Footer from './components/Footer'
 
 function App() {
+  const listItem = [
+    {
+      id: 1,
+      name: 'Home',
+      url: '/',
+    },
+    {
+      id: 2,
+      name: 'About',
+      url: '/About',
+    },
+    {
+      id: 3,
+      name: 'Contact',
+      url: '/Contact',
+    },
+    {
+      id: 4,
+      name: 'Todo',
+      url: '/Todo',
+    },
+  ]
   return (
     <>
-      <Navigation />
+      <Navigation listItem={listItem} />
       <main className="h-screen">
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </main>
-      <Footer></Footer>
+      <Footer/>
     </>
   )
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,22 +1,24 @@
-const Navigation = () => {
+import PropTypes from 'prop-types'
+
+const Navigation = ({ listItem }) => {
   return (
     <nav className="flex justify-between bg-orange-300">
       <h1 className="text-2xl font-bold text-orange-600 flex items-center">
         Logo
       </h1>
       <ul className="flex flex-row gap-8 p-5">
-        <li className="hover:text-orange-600">
-          <a href="">Home</a>
-        </li>
-        <li className="hover:text-orange-600">
-          <a href="">About</a>
-        </li>
-        <li className="hover:text-orange-600">
-          <a href="">Todo</a>
-        </li>
+        {listItem.map((item) => (
+          <li key={item.id} className="hover:text-orange-600">
+            <a href={item.url}>{item.name}</a>
+          </li>
+        ))}
       </ul>
     </nav>
   )
+}
+
+Navigation.propTypes = {
+  listItem: PropTypes.array.isRequired,
 }
 
 export default Navigation
